0965f33e86d8161af0f0c87623af69ef6c445add
Cihan <cihansercaliskan@gmail.com>
2020-02-22 15:35:28 +0300

    Unnecessary codes removed

--- Introduction to Programming/pi_calculator.c
+++ Introduction to Programming/pi_calculator.c
@@ -2,34 +2,22 @@
 
 int main()
 {      
-
 	int i;
 	double p = 0;
 
-
-for ( i = 1; i <= 100; i++)
- {
-
- 
+for ( i = 1; i <= 100; i++){
     if ( i % 2 == 0) 
 	{
-
         p = p - (4.0 / (2 * i - 1));
-
     }
-
 	else
 	{
-		
-	p = p + (4.0 / (2 * i - 1));
-		
+		p = p + (4.0 / (2 * i - 1));
 	}
-
-    printf( "%f\t", p );
-
+    
   }
-
-return 0;
-
+	printf( "%f\t", p );
+	return 0;
+	
+	
 }
-
